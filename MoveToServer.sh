5ad6697e48ccca648cd7ff305da73ffdf250fa3a
swadewhite <44556609+swadewhite@users.noreply.github.com>
2022-01-25 09:39:00 -0500

    Added auto-timestamp in move script

--- MoveToServer.sh
+++ MoveToServer.sh
@@ -1,14 +1,22 @@
+SERVER_DIR="/mnt/Backups/dormTimeLapse" #Path to the NAS where the files will be moved.
+LOCAL_DIR="/var/www/photodump.reshall.rose-hulman.edu/uploads"
 
-# Paths
-SERVER_DIR="/mnt/Backups/dormTimeLapse"                         # Server Storage Location
-LOCAL_DIR="/var/www/photodump.reshall.rose-hulman.edu/uploads"  # Web Server Dropbox
-
-# Dates
-FILE_DATE=$(date +'%Y.%m.%d')    # Ex. 2022.01.23
+FILE_DATE=$(date +'%Y-%m-%d')    # Ex. 2022.01.23
 HUMAN_DATE=$(date +'%B %d, %Y')  # Ex. January 23, 2022
 
-# Make directory if doesn't exist (only at midnight really)
-mkdir -p "$SERVER_DIR/$HUMAN_DATE"
+cp -p "$LOCAL_DIR/"* "$SERVER_DIR/"
+rm -r "$LOCAL_DIR/"*
 
-# Move pictures matching regex to correct date
-mv "$LOCAL_DIR/$FILE_DATE"* "$SERVER_DIR/$HUMAN_DATE"
+cd "$SERVER_DIR/"
+
+for IMAGE in *.jpg; do
+  MOD_DATE=$(stat -c %y $IMAGE | cut -d ' ' -f 1)
+  MOD_TIME=$(stat -c %y $IMAGE | cut -d ' ' -f 2 | cut -d '.' -f 1)
+
+  sudo convert ./$IMAGE -gravity SouthEast -pointsize 48 -fill white -annotate +30+30  "$HUMAN_DATE $MOD_TIME" ./$IMAGE
+
+  mkdir -p "$SERVER_DIR/$MOD_DATE"
+  cp -p ./$IMAGE ./$MOD_DATE/
+  rm ./$IMAGE
+
+done
--- Timelapse.sh
+++ Timelapse.sh
@@ -3,7 +3,7 @@
 SERVER_DIR="/mnt/Backups/dormTimeLapse" #Path to the NAS where the files will be moved.
 LOCAL_DIR="/var/www/photodump.reshall.rose-hulman.edu/uploads"
 
-FILE_DATE=$(date +'%Y.%m.%d')    # Ex. 2022.01.23
+FILE_DATE=$(date +'%Y-%m-%d')    # Ex. 2022.01.23
 HUMAN_DATE=$(date +'%B %d, %Y')  # Ex. January 23, 2022
 
 # TIMELAPSE_DATE=$(date --date="1 day ago" +'%m-%d-%Y')
@@ -15,16 +15,12 @@ SECONDS=0
 # PAYLOAD=" { \"content\": \"Started the time lapse for $(date --date="1 day ago" +'%B %d, %Y').\" }"
 # curl -X POST -H 'Content-Type: application/json' -d "$PAYLOAD" "$WEBHOOK_URL"
 
-# QUALITY=30
-# ENCODE_SPEED='ultrafast'
-
-
 # DATE=$(date --date="1 day ago" +'%B %d, %Y')
 
 # Debug Date
 DATE=$(date +'%B %d, %Y')
 
-ffmpeg -framerate 60 -pattern_type glob -i "$SERVER_DIR/$HUMAN_DATE/*.jpg" -s:v 1600x1200 -c:v libx264 -crf 25 -pix_fmt yuv420p "$SERVER_DIR/$HUMAN_DATE/Dorm Timelapse $TIMELAPSE_DATE.mp4"
+ffmpeg -framerate 60 -pattern_type glob -i "$SERVER_DIR/$FILE_DATE/*.jpg" -s:v 1600x1200 -c:v libx264 -crf 25 -pix_fmt yuv420p "$SERVER_DIR/$FILE_DATE/Dorm Timelapse $TIMELAPSE_DATE.mp4"
 
 
 
