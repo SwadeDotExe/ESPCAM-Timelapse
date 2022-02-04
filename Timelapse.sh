d7fd3f7a9ba8cee163360e7367264d2f8d199f7d
swadewhite <44556609+swadewhite@users.noreply.github.com>
2022-02-03 20:55:27 -0500

    Updates

--- Timelapse.sh
+++ Timelapse.sh
@@ -3,26 +3,22 @@
 SERVER_DIR="/mnt/Backups/dormTimeLapse" #Path to the NAS where the files will be moved.
 LOCAL_DIR="/var/www/photodump.reshall.rose-hulman.edu/uploads"
 
-FILE_DATE=$(date +'%Y-%m-%d')    # Ex. 2022.01.23
-HUMAN_DATE=$(date +'%B %d, %Y')  # Ex. January 23, 2022
+FILE_DATE=$(date --date="1 day ago" +'%Y-%m-%d')    # Ex. 2022.01.23
+HUMAN_DATE=$(date --date="1 day ago" +'%B %d, %Y')  # Ex. January 23, 2022
 
-# TIMELAPSE_DATE=$(date --date="1 day ago" +'%m-%d-%Y')
-TIMELAPSE_DATE=$(date +'%m-%d-%Y')
+TIMELAPSE_DATE=$(date --date="1 day ago" +'%m-%d-%Y')
+#TIMELAPSE_DATE=$(date +'%m-%d-%Y')
 
-# WEBHOOK_URL="https://discord.com/api/webhooks/792932713174663181/dsgVjFAPly0v_8nkJ7B6Fp7XzsaPnHhTyDsIlBhcavE5x7TZW4Mu9ZM3VGBZqPlREgjH"
 SECONDS=0
 
-# PAYLOAD=" { \"content\": \"Started the time lapse for $(date --date="1 day ago" +'%B %d, %Y').\" }"
-# curl -X POST -H 'Content-Type: application/json' -d "$PAYLOAD" "$WEBHOOK_URL"
-
-# DATE=$(date --date="1 day ago" +'%B %d, %Y')
+DATE=$(date --date="1 day ago" +'%B %d, %Y')
 
 # Debug Date
-DATE=$(date +'%B %d, %Y')
+#DATE=$(date +'%B %d, %Y')
 
 ffmpeg -framerate 60 -pattern_type glob -i "$SERVER_DIR/$FILE_DATE/*.jpg" -s:v 1600x1200 -c:v libx264 -crf 25 -pix_fmt yuv420p "$SERVER_DIR/$FILE_DATE/Dorm Timelapse $TIMELAPSE_DATE.mp4"
 
-
+youtube-upload --client-secrets=/mnt/SSD/Scripts/DormTimeLapse/secret.json --title="Dorm Timelapse $TIMELAPSE_DATE" --description="Timelapse from Percopo Hall on $HUMAN_DATE." --playlist="Dorm Timelapses" "$SERVER_DIR/$FILE_DATE/Dorm Timelapse $TIMELAPSE_DATE.mp4"
 
 # Discord Notification
 if (( $SECONDS > 3600 )); then
@@ -41,5 +37,3 @@ else
     echo "Completed in $SECONDS seconds"
     PAYLOAD=" { \"content\": \"The CCTV footage finished the time lapse in $SECONDS seconds. The size after the time lapse is **$PRE_SIZE**.\" }"
 fi
-
-# curl -X POST -H 'Content-Type: application/json' -d "$PAYLOAD" "$WEBHOOK_URL"
